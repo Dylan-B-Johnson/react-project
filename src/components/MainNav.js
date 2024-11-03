3aed8bf68be35250c9ca0ef38f1faaf1046107d6
Dylan-B-Johnson <dmaxbear123@gmail.com>
2024-11-03 17:16:29 -0500

    switched to Link

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,20 +1,21 @@
 import "../css/MainNav.css";
+import {Link} from "react-router-dom";
 
 const MainNav = () => {
   return (
     <nav id="main-nav" className="relative desktop">
         <ul>
             <div className="columns center-columns" id="main-nav-main-div">
-                <li><a href="about"><h2>About</h2></a></li>
-                <li><a href="gallery"><h2>Gallery</h2></a></li>
-                <li><a href="glazes"><h2>Glazes</h2></a></li>
+                <li><Link to="/about"><h2>About</h2></Link></li>
+                <li><Link to="/gallery"><h2>Gallery</h2></Link></li>
+                <li><Link to="/glazes"><h2>Glazes</h2></Link></li>
                 <li id="dropdown-container">
                     <a id="edit"><h2>Edit</h2></a>
                     <div id="edit-dropdown" className="dropdown">
-                        <div className="edit-div"><a href="edit-about"><h2>About</h2></a></div>
-                        <div className="edit-div"><a href="edit-gallery"><h2>Gallery</h2></a></div>
-                        <div className="edit-div"><a href="edit-glazes"><h2>Glazes</h2></a></div>
-                        <div className="edit-div"><a href="edit-users"><h2>Users</h2></a></div>
+                        <div className="edit-div"><Link to="/edit-about"><h2>About</h2></Link></div>
+                        <div className="edit-div"><Link to="/edit-gallery"><h2>Gallery</h2></Link></div>
+                        <div className="edit-div"><Link to="/edit-glazes"><h2>Glazes</h2></Link></div>
+                        <div className="edit-div"><Link to="/edit-users"><h2>Users</h2></Link></div>
                     </div>
                 </li>
             </div>
